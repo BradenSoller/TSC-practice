f902e9b6f0e5ebb82ddf354c891e6a7bcab37ff0
Braden Soller <braden.soller18@gmail.com>
2024-11-07 20:23:43 -0600

    got the id's from my index.html

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 console.log('hello')
+const number1 = document.getElementById('num1') as HTMLInputElement
+const printButton = document.getElementById('printBtn') as HTMLButtonElement
 
-function Add(a: number, b: number) {
-   let sum = 0;
-    return sum = a + b
+
+
+
+
+
+
+
+// function Add(a: number, b: number) {
+//    let sum = 0;
+//     return sum = a + b
     
-}
+// }
 
 
-console.log(Add(3, 2));
+// console.log(Add(3, 2));
 
 
 
