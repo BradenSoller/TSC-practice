c5609bd57dc17eeb5bb55c02c3a175a040cc4a80
Braden Soller <braden.soller18@gmail.com>
2024-11-18 15:07:42 -0600

    created function to convert number to string

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,14 @@ console.log('hello');
 const number1 = document.getElementById("num1");
 const printButton = document.getElementById("printBtn");
 const printValue = document.getElementById("enteredNumber");
+console.log(printButton);
+console.log(printValue);
+function printEnteredValue() {
+    const numb1 = parseFloat(number1.value);
+    printValue.textContent = numb1.toString();
+}
+;
+printButton.addEventListener("click", printEnteredValue);
 // function Add(a: number, b: number) {
 //    let sum = 0;
 //     return sum = a + b
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 console.log('hello')
-const number1 = document.getElementById("num1") as HTMLInputElement
+const number1 = document.getElementById("num1") as HTMLInputElement;
 
-const printButton = document.getElementById("printBtn") as HTMLButtonElement
-const printValue = document.getElementById("enteredNumber") as HTMLOutputElement
+const printButton = document.getElementById("printBtn") as HTMLButtonElement;
+const printValue = document.getElementById("enteredNumber") as HTMLOutputElement;
+console.log(printButton);
+console.log(printValue);
 
 
 
+function printEnteredValue(): void {
+    const numb1 = parseFloat(number1.value);
+    printValue.textContent = numb1.toString();
+};
 
+printButton.addEventListener("click", printEnteredValue);
 
 
 
@@ -16,6 +23,7 @@ const printValue = document.getElementById("enteredNumber") as HTMLOutputElement
 //     return sum = a + b
     
 // }
+
 
 
 // console.log(Add(3, 2));
