b445a170167956a55e9f7c57f4bcbe94e0319c79
Braden Soller <braden.soller18@gmail.com>
2024-11-26 11:46:15 -0600

    finally got project to work

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 console.log('hell')
-const number1 = document.getElementById("num1") as HTMLInputElement;
 
-const printButton = document.getElementById("printBtn") as HTMLButtonElement;
-const printValue = document.getElementById("enteredNumber") as HTMLOutputElement;
-console.log(printButton);
-console.log(printValue);
+    
+
+    const number1 = document.getElementById("num1") as HTMLInputElement;
+    const printButton = document.getElementById("printBtn") as HTMLButtonElement;
+    const printValue = document.getElementById("enteredNumber") as HTMLOutputElement;
+    console.log(printButton);
+    console.log(printValue);
 
 
 
-function printEnteredValue(): void {
-    const numb1 = parseFloat(number1.value);
-    printValue.textContent = numb1.toString();
-};
+    function printEnteredValue(): void {
+        const numb1 = parseFloat(number1.value);
+        printValue.textContent = numb1.toString();
+    };
 
-printButton.addEventListener("click", printEnteredValue);
+    printButton.addEventListener("click", printEnteredValue);
 
 
 
