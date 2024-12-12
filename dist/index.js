2b23468759b232b9cf84895e27068da05c60d9c0
Braden Soller <braden.soller18@gmail.com>
2024-12-12 12:54:11 -0600

    added styling

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,7 @@
 "use strict";
-var _a;
-console.log('hell');
-const number1 = document.getElementById("num1");
-const printButton = document.getElementById("printBtn");
-const printValue = document.getElementById("enteredNumber");
-const Timeout = document.getElementById("timeOut");
-const printHi = document.getElementById("braden");
-console.log(printButton);
-console.log(printValue);
-console.log(Timeout);
-console.log(printHi);
-function printEnteredValue() {
-    const numb1 = parseFloat(number1.value);
-    printValue.textContent = numb1.toString();
-}
-;
-printButton.addEventListener("click", printEnteredValue);
-function sayHi() {
-    setTimeout(() => {
-        console.log("hi");
-    }, 3000);
-}
-// sayHi()
-// Timeout.addEventListener("wait", sayHi);
-printHi.addEventListener("say", sayHi);
-(_a = document.getElementById("timeOut")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", sayHi
-// Trigger the timeout when button is clicked
-// qeustion mark triggers optional chaining to avoid runtime errors
-);
-// function Add(a: number, b: number) {
-//    let sum = 0;
-//     return sum = a + b
-// }
-// console.log(Add(3, 2));
+let firstname = document.getElementById('firstNameInput');
+let lastname = document.getElementById('lastNameInput');
+let ID = document.getElementById('idInput');
+let title = document.getElementById('titleInput');
+let annualSalary = document.getElementById('annualSalary');
 //# sourceMappingURL=index.js.map
---
+++ src/index.js
@@ -0,0 +1,5 @@
+var firstname = document.getElementById('firstNameInput');
+var lastname = document.getElementById('lastNameInput');
+var ID = document.getElementById('idInput');
+var title = document.getElementById('titleInput');
+var annualSalary = document.getElementById('annualSalary');
