db21ad756465c37a2296569e7010384ed6bd9471
Braden Soller <braden.soller18@gmail.com>
2024-12-12 12:42:18 -0600

    added inputs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+let firstname = document.getElementById('firstNameInput') as HTMLInputElement;
+let lastname = document.getElementById('lastNameInput') as HTMLInputElement;
+let ID = document.getElementById('idInput') as HTMLInputElement;
+let title = document.getElementById('titleInput') as HTMLInputElement;
+let annualSalary = document.getElementById('annualSalary') as HTMLInputElement;
+
 
