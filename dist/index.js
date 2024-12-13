2db9fddb1cc384a36d28997375cfa0b9b22364a4
Braden Soller <braden.soller18@gmail.com>
2024-12-13 15:36:52 -0600

    added monthly cap and table elements

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,34 @@
 "use strict";
-let firstname = document.getElementById('firstNameInput');
-let lastname = document.getElementById('lastNameInput');
-let ID = document.getElementById('idInput');
-let title = document.getElementById('titleInput');
-let annualSalary = document.getElementById('annualSalary');
+let monthlyCap = 0;
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('employeeForm');
+    form.addEventListener('submit', handleSubmit); // Event listener to handle form submission
+});
+function handleSubmit(event) {
+    event.preventDefault();
+    let firstName = document.getElementById('firstNameInput');
+    let lastName = document.getElementById('lastNameInput');
+    let ID = document.getElementById('idInput');
+    let title = document.getElementById('titleInput');
+    let annualSalary = document.getElementById('annualSalaryInput');
+    const element = document.getElementById('over-budget');
+    const table = document.getElementById('tableBody');
+    table.innerHTML += `<tr>
+    <td>${firstName.value}</td>
+    <td>${lastName.value}</td>
+    <td>${ID.value}</td>
+    <td>${title.value}</td>
+    <td>${annualSalary.value}</td>
+    <td><button onclick="deleteText(event)">delete</button></td>
+     </tr>`;
+    //clear inputs 
+    firstName.value = "";
+    lastName.value = "";
+    ID.value = "";
+    title.value = "";
+    annualSalary.value = "";
+    if (monthlyCap > 20000) {
+        element.style.color = "red";
+    }
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
-let firstname = document.getElementById('firstNameInput') as HTMLInputElement;
-let lastname = document.getElementById('lastNameInput') as HTMLInputElement;
-let ID = document.getElementById('idInput') as HTMLInputElement;
-let title = document.getElementById('titleInput') as HTMLInputElement;
-let annualSalary = document.getElementById('annualSalary') as HTMLInputElement;
+let monthlyCap:any = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('employeeForm') as HTMLFormElement;
+    form.addEventListener('submit', handleSubmit); // Event listener to handle form submission
+});
+
+function handleSubmit(event: Event):void {
+
+    event.preventDefault();
+
+    let firstName = document.getElementById('firstNameInput') as HTMLInputElement;
+    let lastName = document.getElementById('lastNameInput') as HTMLInputElement;
+    let ID = document.getElementById('idInput') as HTMLInputElement;
+    let title = document.getElementById('titleInput') as HTMLInputElement;
+    let annualSalary = document.getElementById('annualSalaryInput') as HTMLInputElement;
+    const element = document.getElementById('over-budget') as HTMLElement;
 
 
+    const table = document.getElementById('tableBody') as HTMLTableElement
+    table.innerHTML += `<tr>
+    <td>${firstName.value}</td>
+    <td>${lastName.value}</td>
+    <td>${ID.value}</td>
+    <td>${title.value}</td>
+    <td>${annualSalary.value}</td>
+    <td><button onclick="deleteText(event)">delete</button></td>
+     </tr>`
+    
+    //clear inputs 
+    firstName.value = "";
+    lastName.value = "";
+    ID.value = "";
+    title.value = "";
+    annualSalary.value = "";
+
+    if (monthlyCap > 20000) {
+        element.style.color = "red"
+    }
+}
